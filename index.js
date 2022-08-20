20e9694e4de9614a6f7fdd25b4ac804abe80586f
RealJace <72617776+Simonek-loki@users.noreply.github.com>
2022-08-20 12:40:55 +0200

    new token

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const path = require("path");
 const fs = require("fs")
 const Eris = require("eris");
@@ -15,11 +17,11 @@ var commandsArray = [];
 
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	
+
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-			
+
 	commands[command.name] = command
 	commandsArray.push(command)
 }
@@ -45,11 +47,11 @@ client.on("ready",async () => {
 		}
 	});
 	}
-	
+
 	setStatus()
 
 	setInterval(setStatus,500)
-	
+
 	if (process.env.GUILD_COMMANDS === "False") {
 		await client.bulkEditCommands(commandsArray);
 		console.log("Registered commands globally.");
@@ -57,7 +59,7 @@ client.on("ready",async () => {
 		await client.bulkEditGuildCommands(process.env.GUILD,commandsArray);
 		console.log("Registered commands in guild.");
 	}
-	
+
 	console.log("Bot is ready.")
 })
 
@@ -71,7 +73,7 @@ client.on("interactionCreate",async interaction => {
 		if (!interaction.guildID) return interaction.createMessage("this bot is designed to be used in guilds, so use me in guilds please");
 
 		var oldData = data.get();
-		
+
 		if (interaction.member !== undefined) {
 			if (oldData[interaction.member.id] === null || oldData[interaction.member.id] === undefined) {
 				oldData[interaction.member.id] = {};
@@ -82,7 +84,7 @@ client.on("interactionCreate",async interaction => {
 		}
 
 		data.set(oldData);
-		
+
 		if (commands[interaction.data.name]) {
 			try {
 				await commands[interaction.data.name].execute(interaction,client);
@@ -95,15 +97,15 @@ client.on("interactionCreate",async interaction => {
 
 	if (interaction instanceof Eris.ComponentInteraction) {
 		if (interaction.data.component_type === 2 && interaction.data.custom_id.includes("anothermeme_")) {
-			
+
 			var subreddit = interaction.data.custom_id.split("_")[1]
-			
+
 			if (subreddit !== undefined) {
 
 				const data = await axios.get(`https://meme-api.herokuapp.com/gimme/${subreddit}`)
 
 				const meme = data.data
-				
+
 				var embed = {
 					title: meme.title,
 					description: `posted in r/${meme.subreddit}`,
@@ -122,7 +124,7 @@ client.on("interactionCreate",async interaction => {
 				};
 
 				await interaction.acknowledge()
-				
+
 				interaction.editOriginalMessage({
 					embeds: [embed],
 					components: [
