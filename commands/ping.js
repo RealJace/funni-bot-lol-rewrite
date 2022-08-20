09d6fa2172054fd86d226a4b4207fabb44e2e9ab
RealJace <72617776+Simonek-loki@users.noreply.github.com>
2022-08-21 01:41:35 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,12 @@ module.exports = {
 	name: "ping",
 	description: "gain my latency",
 	options: [],
-	async execute(interaction) {
+	async execute(interaction,client) {
 		if (!(interaction instanceof Eris.CommandInteraction)) return;
+
+		const guild = client.guilds.get(interaction.guildID)
+
+		interaction.createMessage(`${guild.shard.latency}ms`)
+
 	}
 }
